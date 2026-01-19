19f6dcbe8512e75e667f86eeab7afb9966a39b55
Luckysisodiya170 <luckysisodiya170@gmail.com>
2026-01-19 04:36:43 +0000

    Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+/* ===== PROTECTED ROUTE (INLINE) ===== */
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("adminToken");
+  return token ? children : <Navigate to="/" replace />;
+};
+
 function App() {
   return (
     <BrowserRouter>
-      {/* 🔔 TOAST CONTAINER (GLOBAL) */}
       <ToastContainer
         position="top-right"
         autoClose={3000}
-        hideProgressBar={false}
         newestOnTop
-        closeOnClick
-        pauseOnHover
-        draggable
         theme="colored"
       />
 
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
